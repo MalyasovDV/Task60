638d9c2c098df779e3e7b6bff5dc22a9e88a41e6
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-06 14:31:35 +0800

    Task done

--- Program.cs
+++ Program.cs
@@ -14,11 +14,17 @@
 void MatrixInitialization(ref int[,,] matrix)
 {
     Random rnd = new Random();
+    int[] count = new int[100];
 
     for (int i = 0; i < matrix.GetLength(0); ++i)
         for (int j = 0; j < matrix.GetLength(1); ++j)
             for (int k = 0; k < matrix.GetLength(2); ++k)
-                matrix[i, j, k] = rnd.Next(10, 99);
+                {
+                    matrix[i, j, k] = rnd.Next(10, 99);
+                    while (count[matrix[i, j, k]] > 0)
+                        matrix[i, j, k] = rnd.Next(10, 99);
+                    count[matrix[i, j, k]]++;
+                }
                 
         
 }
